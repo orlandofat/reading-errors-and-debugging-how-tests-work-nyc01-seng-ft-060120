1f14bf07b306c644cb1bad5f949658c019050bb3
Orlando Challenger <orlandofat@gmail.com>
2020-05-07 18:14:25 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+
+puts first_number = 5
+puts second_number = 2.0
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
