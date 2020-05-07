755432774f122f4540a10b4bd0897f4d019ba96d
Orlando Challenger <orlandofat@gmail.com>
2020-05-07 18:02:45 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,3 +4,5 @@ puts first_number = 5
 puts second_number = 2.0
 sum = first_number + second_number
 sum = first_number - second_number
+sum = first_number * second_number
+sum = first_number / second_number
